fca9ff33aa5da7ed86a761347754068c2c913fe4
slow <tombraun87@gnail.com>
2021-11-11 22:24:01 +0100

    Add more defines and comment stuff.

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,40 @@
 #include <sstream>
 #include "c_sdlhelper.h"
 
-#undef main
+#undef main // some stupid sdl2 shit
 
 namespace arguments {
     namespace pos {
         int output = -1,
                 font = -1,
-                fontsize = -1;
+                font_size = -1;
     }
     namespace available {
         bool output = false,
                 font = false,
-                fontsize = false;
+                font_size = false;
     }
 }
 
-#define ADD_ARG(name, variable) \
+#define ADD_ARG(ns, name, var) \
     if (std::strcmp(argv[i], (name)) == 0) { \
-        (variable) = i + 1; \
+        (ns::pos::var) = i + 1; \
         continue; \
     } \
-    if ((variable) != -1) { \
-        if (i == (variable)) continue; \
+    if ((ns::pos::var) != -1) { \
+        if (i == (ns::pos::var)) continue; \
     } \
 
-#define CREATE_ARG_BOOL(name, value) (name) = ((value) != -1)
+#define CREATE_ARG_BOOL(ns, name) (ns::available::name) = ((ns::pos::name) != -1)
+
+// just because i now decided that i enjoy the abusement of defines in cpp
+#define SET_AFTER_ARG_STR(ns, name, var) \
+    if (ns::available::name)                \
+        var = argv[ns::pos::name]; \
+
+#define SET_AFTER_ARG_INT(ns, name, var) \
+    if (ns::available::name)                \
+        var = atoi(argv[ns::pos::name]); \
 
 
 int main(int argc, char *argv[]) {
@@ -38,18 +47,16 @@ int main(int argc, char *argv[]) {
     std::stringstream text_list;
 
     {
-        using namespace arguments;
-
         for (int i = 1; i < argc; i++) {
-            ADD_ARG("-o", pos::output)
-            ADD_ARG("-f", pos::font)
-            ADD_ARG("-fs", pos::fontsize)
+            ADD_ARG(arguments, "-o", output)
+            ADD_ARG(arguments, "-f", font)
+            ADD_ARG(arguments, "-fs", font_size)
             text_list << argv[i] << " ";
         }
 
-        CREATE_ARG_BOOL(available::output, pos::output);
-        CREATE_ARG_BOOL(available::font, pos::font);
-        CREATE_ARG_BOOL(available::fontsize, pos::fontsize);
+        CREATE_ARG_BOOL(arguments, output);
+        CREATE_ARG_BOOL(arguments, font);
+        CREATE_ARG_BOOL(arguments, font_size);
     }
 
     std::string text = text_list.str();
@@ -60,16 +67,11 @@ int main(int argc, char *argv[]) {
     int font_size = 100;
 
     {
-        if (arguments::available::output)
-            bmp_path = argv[arguments::pos::output];
-
-        if (arguments::available::font)
-            font_path = argv[arguments::pos::font];
-
-        if (arguments::available::fontsize)
-            font_size = atoi(argv[arguments::pos::fontsize]);
+        SET_AFTER_ARG_STR(arguments, output, bmp_path)
+        SET_AFTER_ARG_STR(arguments, font, font_path)
+        SET_AFTER_ARG_INT(arguments, font_size, font_size)
     }
-
+    
     sdl.load_font(font_path, font_size);
     sdl.write_text_to_bmp(text, bmp_path);
 
